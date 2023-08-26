a70e4f2cf323fb76322724ab0551a1e9af4d0402
Gustavo <gustavo_roger@yahoo.com>
2023-08-26 11:05:22 -0700

    refactored code in icons component to simplify it and make it more readable

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,20 +1,31 @@
-// Icon Component
+// Icon Component with icon details props passed from UserBox component
 import React from 'react'
 
 function Icons ({iconDetails}) {
 
+    // function that will determine the className of each icon to determine the color depending on the iconDetails prop value that will change with every user
+    const setClass = (detail) => {
+        // values are as follows, (0 = green/good, 1 = yellow/average, 2 red/needs improvement)
+        if (detail === 0) {
+            return 'icon green'
+        } else if (detail === 1) {
+            return 'icon yellow'
+        } else 
+        return 'icon red'
+    }
+
     return (
         <div className='iconContainer'>
 
-            <div className={ iconDetails[0] === 0 ? 'icon green' : iconDetails[0] === 1 ? 'icon yellow' : 'icon red'}>
+            <div className={ setClass(iconDetails[0]) }>
                 <img src='images/home.png' alt='home'/>
             </div>
             
-            <div className={ iconDetails[1] === 0 ? 'icon green' : iconDetails[1] === 1 ? 'icon yellow' : 'icon red'}>
+            <div className={ setClass(iconDetails[1]) }>
                 <img src='images/drink.png' alt='food'/>
             </div>
 
-            <div className={ iconDetails[2] === 0 ? 'icon green' : iconDetails[2] === 1 ? 'icon yellow' : 'icon red'}>
+            <div className={ setClass(iconDetails[2]) }>
                 <img src='images/heart.png' alt='health'/>
             </div>
         </div>
