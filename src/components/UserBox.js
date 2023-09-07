4442e3c7aedc8bbf29e73484c3a8619de5b94aa0
Gustavo <gustavo_roger@yahoo.com>
2023-09-06 22:27:11 -0700

    changed styling to add hover effect, amdn changed userBox container from div to main

--- src/components/UserBox.js
+++ src/components/UserBox.js
@@ -3,12 +3,12 @@ import Icons from './Icons'
 
 function UserBox () {
     // users array (Temporary Placeholder Values)
-    // for each value in users array value[0] = username & value[1] = array containing the details of each user in the order of home, meals, and heart health or heart rate where 0 = good / 1 = okay / 2 = needs improvement
+    // for each value in users array, value[0] = username, & value[1] = array containing the details of each user in the order of home, meals, and heart health or heart rate where 0 = good / 1 = okay / 2 = needs improvement
     // depending on (Actual) data the values will be adjusted and gathered via fetch request to server.
     const users = [['Lois Lane', [0, 0, 2]], ['Mary Jane', [1 , 2, 0]], ['Padme Amalda', [0, 2, 1]], ['Christine Chapel', [2, 1, 0]]]
 
     return (
-        <div id='userBox'>
+        <main id='userBox'>
             {/* User container with username and other details */}
             {users.map((user, index) => (
                 <div id='userDetails' key={index}> 
@@ -16,7 +16,7 @@ function UserBox () {
                     <Icons iconDetails={user[1]}/>
                 </div>
             ))}
-        </div>
+        </main>
     )
 }
 
