cc01cba097f9d5333d2c73a54d7dbec384f8001d
Gustavo <gustavo_roger@yahoo.com>
2023-09-10 21:57:03 -0700

    logic to conditional that determines expansion of the details on click corrected and implemented

--- src/components/UserBox.js
+++ src/components/UserBox.js
@@ -8,24 +8,34 @@ function UserBox () {
     // depending on (Actual) data the values will be adjusted and gathered via fetch request to server.
     const users = [['Lois Lane', [0, 0, 2]], ['Mary Jane', [1 , 2, 0]], ['Padme Amalda', [0, 2, 1]], ['Christine Chapel', [2, 1, 0]]]
     // state variable to determine expansion/collapse
-    let [expanded, setExpanded] = useState(false);
+    let [collapse, setCollapse] = useState(true);
     // state variable to track the user index upon clicking a specific user
     let [userIndex, setUserIndex] = useState(null);
+
+    const expand = (index) => {
+        if (userIndex === null) {
+            setUserIndex(index)
+            setCollapse(false)
+        } else if (userIndex !== null) {
+            if ( index === userIndex) {
+                setCollapse(true)
+            } else if ( index !== userIndex) {
+                setCollapse(false)
+                setUserIndex(index)
+            }
+        }
+    }
     
     return (
         <div id='userBox'>
             {/* User container with username and other details */}
             {users.map((user, index) => (
-                <div id='userDetails' onClick={() => {
-                    console.log(user[0]+" Clicked");
-                    expanded ? setExpanded(false) : setExpanded(true);
-                    setUserIndex(index)
-                }} key={index} > 
-                    <div className='userFlexBlock'>
+                <div id='userDetails' key={index} > 
+                    <div className='userFlexBlock' onClick={() => expand(index)}>
                         <h1>{user[0]}</h1>
                         <Icons iconDetails={user[1]}/>
                     </div>
-                    {!expanded && index === userIndex ? <Details/> : null}
+                    {!collapse && index === userIndex ? <Details/> : null}
                 </div>
             ))}
         </div>
